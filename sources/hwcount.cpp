996e475954b93eb8adb8563ed7afd8182b9811df
hobywan <hoby.rakotoarivelo@gmail.com>
2017-02-09 19:20:12 +0100

    PAPI counters wrapper

---
+++ sources/hwcount.cpp
@@ -0,0 +1,368 @@
+/**
+ * Implementation of papi-wrapper library.
+ */
+#ifdef HAVE_PAPI
+#include "papi_t.h"
+
+/* ------------------------------------ */
+void papi_t::start( std::vector<papi_t*> &counter_array ) {
+	#pragma omp parallel num_threads( counter_array.size() )
+	{
+		counter_array[ omp_get_thread_num() ]->start();
+	}
+}
+/* ------------------------------------ */
+void papi_t::stop( std::vector<papi_t*> &counter_array ){
+	#pragma omp parallel num_threads( counter_array.size() )
+	{
+		counter_array[ omp_get_thread_num() ]->stop();
+	}
+}
+/* ------------------------------------ */
+void papi_t::sum( std::vector<papi_t*> &counter_array ){
+	for( uint32_t i = 1; i < counter_array.size(); ++i ) {
+		*( counter_array[ 0 ] ) += *( counter_array[ i ] );
+	}
+}
+/* ------------------------------------ */
+std::ostream& operator<<(std::ostream& os, const papi_t & b)
+{
+	b.to_stream( os );
+	return os;
+}
+
+/* -- print --------------------------- */
+
+void papi_branch::to_stream( std::ostream& os ) const
+{
+  os << branch_hits() << "\t" << branch_misses() << "\t" << branch_instructions() 
+  	<< "\t" << misprediction_ratio();
+}
+/* ------------------------------------ */
+void papi_cycles::to_stream( std::ostream& os ) const
+{
+  os << instructions() << "\t" << cycles() << "\t" << cycles_per_instruction();
+}
+
+/* ------------------------------------ */
+void papi_instructions::to_stream( std::ostream& os ) const
+{
+  os << load_instructions_ratio() << "\t" << store_instructions_ratio() 
+  	<< "\t" << branch_instructions_ratio();
+}
+/* ------------------------------------ */
+void papi_tlb::to_stream( std::ostream& os ) const 
+{
+  os << data_tlbs() << "\t" << instruction_tlbs();
+}
+/* ------------------------------------ */
+void papi_cache::to_stream( std::ostream& os ) const
+{
+	//first output L2 statistics: misses, accesses, and miss ratio
+	//then output L3 statistics: misses, accesses, and miss ratio
+	os << l2_cache_misses() << "\t" << l2_cache_accesses() << "\t" << l2_miss_ratio() 
+	   << "\t"
+	   << l3_cache_misses() << "\t" << l3_cache_accesses() << "\t" << l3_miss_ratio();
+}
+/* ------------------------------------ */
+void papi_custom::to_stream( std::ostream& os ) const
+{
+	if( values_.size() > 0 ) {
+		os << values_[0];
+		for( uint32_t i = 1; i < values_.size(); ++i ) {
+			os << "\t" << values_[ i ];
+		}
+	}
+}
+
+/* ---------- helper functions -------- */
+long long absolute_difference( const long long v1, const long long v2 )
+{
+	return ( v1 > v2 ? v1 - v2 : v2 - v1 );
+}
+/* ------------------------------------ */
+void handle_error( int retval ) {
+	std::cerr << "PAPI error " << retval << ": " << PAPI_strerror( retval ) << std::endl;
+	exit(1);
+}
+
+//--------- Base class implementation -------------//
+void papi_t::register_counter(const std::string &counter_name, const std::string &header)
+{
+	int counter;
+	char c_style_string[ counter_name.length() + 1 ];
+	strcpy( c_style_string, counter_name.c_str() );
+	c_style_string[ counter_name.length() ] = '\0';
+	int retval = PAPI_event_name_to_code( c_style_string, &counter );
+	if ( retval != PAPI_OK ) {
+		std::cerr << "Could not decode PAPI counter: " << counter_name << std::endl;
+		return;
+	}
+	else { 
+		values_.push_back( 0 );
+		counters_.push_back( counter );
+		headers_.push_back( header );
+	}
+}
+/* ------------------------------------ */
+void papi_t::reset()
+{
+	for( auto it = values_.begin(); it != values_.end(); ++it ) {
+		*it = 0;
+	}
+}
+/* ------------------------------------ */
+void papi_t::start()
+{
+	std::vector<int> eventsMutable( counters_.data(), counters_.data() + counters_.size() );
+	int retval = PAPI_start_counters( &eventsMutable[ 0 ], counters_.size() );
+	if (retval == PAPI_OK) {
+		papi_started_ = true;
+	} else {
+		std::cerr << "PAPI error " << retval << ": " << PAPI_strerror( retval ) << std::endl;
+		papi_started_ = false;
+	}
+}
+/* ------------------------------------ */
+void papi_t::stop( ) {
+	
+  if( values_.size() == 0 ) { return; }
+	if( papi_started_ ) {
+		long long v[ counters_.size() ];
+		int retval = PAPI_stop_counters( &v[0], counters_.size() );
+		if( retval != PAPI_OK ) handle_error( retval );
+		for( uint32_t i = 0; i < values_.size(); ++i ) {
+			values_[ i ] += v[ i ];
+		}
+	}
+	else {
+		for ( auto it = values_.begin(); it != values_.end(); ++it ) {
+			*it = -1;
+		}
+	}
+	papi_started_ = false;
+}
+/* ------------------------------------ */
+std::string papi_t::headers() {
+
+	if( headers_.size() == 0 ) { return ""; }
+	std::string output( headers_[ 0 ] );
+	for( uint32_t i = 1; i < headers_.size(); ++i ) {
+		output = output + "\t" + headers_[ i ];
+	}
+	return output;
+}
+/* ------------------------------------ */
+papi_t & papi_t::operator=(const papi_t & other) {
+	for( uint32_t i = 0; i < values_.size(); ++i ) {
+		values_[ i ] = other.values_[ i ];
+	}
+	return *this;
+}
+/* ------------------------------------ */
+papi_t & papi_t::operator+=(const papi_t & other) {
+	for( uint32_t i = 0; i < values_.size(); ++i ) {
+		values_[ i ] += other.values_[ i ];
+	}
+	return *this;
+}
+/* ------------------------------------ */
+papi_t & papi_t::operator-=(const papi_t & other) {
+	for( uint32_t i = 0; i < values_.size(); ++i ) {
+		values_[ i ] = absolute_difference( values_[ i ], other.values_[ i ] );
+	}
+	return *this;
+}
+/* ------------------------------------ */
+papi_t & papi_t::operator/=(const uint32_t scalar) {
+	for( auto it = values_.begin(); it != values_.end(); ++it ) {
+		*it /= scalar;
+	}
+	return *this;
+}
+/* ------------------------------------ */
+papi_t & papi_t::operator*=(const uint32_t scalar) {
+	for( auto it = values_.begin(); it != values_.end(); ++it ) {
+		*it *= scalar;
+	}
+	return *this;
+}
+
+//--------- Predefined classes implementation -------------//
+
+papi_instructions::papi_instructions() {
+	this->register_counter( std::string( "PAPI_LD_INS" ), std::string( "loads" ) ); //NOT on AMD
+	this->register_counter( std::string( "PAPI_SR_INS" ), std::string( "stores" ) ); //NOT on AMD
+	this->register_counter( std::string( "PAPI_BR_INS" ), std::string( "branches" ) );
+	this->register_counter( std::string( "PAPI_TOT_INS" ), std::string( "total" ) );
+}
+/* ------------------------------------ */
+long long inline papi_instructions::load_instructions() const { return values_[0]; }
+long long inline papi_instructions::store_instructions() const { return values_[1]; }
+long long inline papi_instructions::branch_instructions() const { return values_[2]; }
+long long inline papi_instructions::total_instructions() const { return values_[3]; }
+/* ------------------------------------ */
+double papi_instructions::load_instructions_ratio() const {
+	return load_instructions() / (double) total_instructions();
+}
+/* ------------------------------------ */
+double papi_instructions::store_instructions_ratio() const {
+	return store_instructions() / (double) total_instructions();
+}
+/* ------------------------------------ */
+double papi_instructions::branch_instructions_ratio() const {
+	return branch_instructions() / (double) total_instructions();
+}
+
+/* ------------------------------------ */
+papi_cycles::papi_cycles() {
+	this->register_counter("PAPI_STL_ICY", "cycles_no_instructions_issue");
+	this->register_counter("PAPI_FUL_CCY", "cycles_max_instructions_completed"); //NOT on AMD
+	this->register_counter("PAPI_RES_STL", "cycles_stalled_any_resource");
+	this->register_counter("PAPI_TOT_CYC", "total_cycles");
+	this->register_counter("PAPI_TOT_INS", "total_instructions");
+}
+/* ------------------------------------ */
+inline long long papi_cycles::idle_cycles() const { return values_[ 0 ]; }
+inline long long papi_cycles::utilised_cycles() const { return values_[ 1 ]; }
+inline long long papi_cycles::stalled_cycles() const { return values_[ 2 ]; }
+inline long long papi_cycles::cycles() const { return values_[ 3 ]; }
+inline long long papi_cycles::instructions() const { return values_[ 4 ]; }
+/* ------------------------------------ */
+double papi_cycles::stalled_cycles_ratio() const {
+	return stalled_cycles() / (double) cycles();
+}
+/* ------------------------------------ */
+double papi_cycles::idle_cycles_ratio() const {
+	return idle_cycles() / (double) cycles();
+}
+/* ------------------------------------ */
+double papi_cycles::utilised_cycles_ratio() const {
+	return utilised_cycles() / (double) cycles();
+}
+/* ------------------------------------ */
+double papi_cycles::cycles_per_instruction() const {
+	return cycles() / (double) instructions();
+}
+
+/* ------------------------------------ */
+papi_cache::papi_cache() {
+	this->register_counter("PAPI_L2_TCM", "L2_total_cache_misses");
+	this->register_counter("PAPI_L2_TCA", "L2_total_cache_accesses");
+	this->register_counter("PAPI_L3_TCM", "L3_total_cache_misses");
+	this->register_counter("PAPI_L3_TCA", "L3_total_cache_accesses");
+}
+/* ------------------------------------ */
+long long inline papi_cache::l2_cache_misses() const { return values_[ 0 ]; }
+long long inline papi_cache::l2_cache_accesses() const { return values_[ 1 ]; }
+long long inline papi_cache::l3_cache_misses() const { return values_[ 2 ]; }
+long long inline papi_cache::l3_cache_accesses() const { return values_[ 3 ]; }
+/* ------------------------------------ */
+double papi_cache::l2_miss_ratio() const {
+	return l2_cache_misses() / (double) l2_cache_accesses();
+}
+/* ------------------------------------ */
+double papi_cache::l3_miss_ratio() const {
+	return l3_cache_misses() / (double) l3_cache_accesses();
+}
+/* ------------------------------------ */
+
+
+papi_branch::papi_branch() {
+	this->register_counter( "PAPI_BR_PRC", "conditional_branches_correctly_predicted"); //NOT on AMD
+	this->register_counter( "PAPI_BR_MSP", "conditional_branches_mispredicted");
+}
+
+/* ------------------------------------ */
+long long inline papi_branch::branch_instructions() const { return values_[ 0 ] + values_[ 1 ]; }
+long long inline papi_branch::branch_misses() const { return values_[ 1 ]; }
+long long inline papi_branch::branch_hits() const { return values_[ 0 ]; }
+/* ------------------------------------ */
+double papi_branch::misprediction_ratio() const {
+	return branch_misses() / (double) branch_instructions();
+}
+/* ------------------------------------ */
+double papi_branch::prediction_ratio() const {
+	return branch_hits() / (double) branch_instructions();
+}
+
+/* ------------------------------------ */
+papi_tlb::papi_tlb() {
+	this->register_counter( "PAPI_TLB_DM", "dtlb_misses" );
+	this->register_counter( "PAPI_TLB_IM", "itlb_misses" );
+}
+/* ------------------------------------ */
+long long inline papi_tlb::data_tlbs() const { return values_[ 0 ]; }
+long long inline papi_tlb::instruction_tlbs() const { return values_[ 1 ]; }
+
+/* ------------------------------------ */
+papi_custom::papi_custom( const std::vector< std::pair<std::string,std::string> > &events)
+{
+	for( auto it = events.begin(); it != events.end(); ++it ) {
+		this->register_counter( it->first, it->second );
+	}
+}
+
+/**
+ * Preprocessing stage
+ * @param num_cores the number of cores (1 thread/core)
+ * @param papi_mode the type of papi counter to track
+ * @param papi_counters array of polymorphic papi counter sets per thread and per kernel
+ */
+void trigen::papi_init(const uint32_t num_cores,
+                       const uint32_t papi_mode,
+                       std::vector<papi_t*> hw_counters[4]){
+  assert(num_cores);
+  assert(hw_counters != nullptr);
+  // init random seed
+  srand( (unsigned) time(0) );
+
+  // Initialize PAPI library for each thread.
+  if( PAPI_is_initialized() == PAPI_NOT_INITED ){
+    PAPI_library_init( PAPI_VER_CURRENT );
+#pragma omp parallel num_threads(num_cores)
+    if( PAPI_thread_init( pthread_self ) != PAPI_OK ) {
+      exit(0);
+    }
+  }
+
+  // step 2: initialise and start the papi counter sets, one for each thread
+  if( papi_mode != papi_t::papi_mode_off ) {
+    for(int k=0; k < 4; ++k){
+      for(uint32_t t=0; t < num_cores; ++t) {
+	      switch( papi_mode ){
+          case papi_t::papi_mode_cache :
+            hw_counters[k].push_back( new papi_cache() );
+            break;
+
+          case papi_t::papi_mode_branch :
+            hw_counters[k].push_back( new papi_branch() );
+            break;
+            
+          case papi_t::papi_mode_cycle :
+            hw_counters[k].push_back( new papi_cycles() );
+            break;
+            
+          case papi_t::papi_mode_tlb :
+            hw_counters[k].push_back( new papi_tlb() );
+            break;
+        }
+      }
+    }
+  } 
+}
+
+/**
+ * Report the PAPI counters' values, reduce over all threads, and clean up.
+ */
+void trigen::papi_finalize(std::vector< papi_t* > hw_counters[4]){
+  for(int k=0; k < 4; ++k){
+    // 1: reduce counters for each kernel
+    papi_t::sum( hw_counters[k] );
+    std::cout << *( hw_counters[k][0] ) << std::endl;
+    // 2: clean up
+    while(!hw_counters[k].empty())
+      hw_counters[k].pop_back();   // delete performed here	    
+  }
+}
+#endif
---
+++ sources/hwcount.h
@@ -0,0 +1,281 @@
+/**
+ * Library for updating/accessing standard PAPI counters
+ *
+ * @note The first time you use this library on a new architecture, it may be
+ * worth running \c papi_avail and \c papi_native_avail to determine which 
+ * hardware counters are available on the machine that you are using.
+ */
+#pragma once
+
+#ifdef HAVE_PAPI
+#include <header.h>
+#include <papi.h>
+
+/**
+ * The base class that defines the general behaviour related to any
+ * set of hardware counters.
+ * @tparam NUM_COUNTERS The number of hardware counters used in the given subset.
+ *
+ * A papi_t object defines all the methods related to hardware
+ * counters without knowing how many there are (the template parametre
+ * NUM_COUNTERS) and without knowing what they are. These are specialised
+ * within the specific subclasses.
+ */
+class papi_t {
+
+public:
+	
+	virtual ~papi_t() {}
+
+	/** Starts tracking/reset all the hardware counters in this subset. */
+	void start();
+	/** Resets all counters to zero. */
+	void reset();
+	/**
+	 * Stops tracking all the hardware counters in this subset.
+	 * @post The values accessible in this object have been updated
+	 * to reflect the change in the actual, physical hardware counters
+	 * since start() was invoked.
+	 * @note Without calling stop(), none of the member variables of
+	 * this object will have been updated.
+	 */
+	void stop();
+
+	/**
+	 * Returns a string of tab-separated human-readable names
+	 * for this set of counters, in the same order that the
+	 * counters would be printed to an output stream.
+	 * @return The string of tab-separated headers.
+	 */
+	std::string headers();
+	
+	// operator overloads
+	papi_t & operator= (const papi_t & other);
+	papi_t & operator+=(const papi_t & other);
+	papi_t & operator-=(const papi_t & other);
+	papi_t & operator/=(const uint32_t scalar);
+	papi_t & operator*=(const uint32_t scalar);
+	
+	// static functions
+
+	/**
+	 * Starts up one set of PAPI counters for each thread.
+	 * @param counter_array An array of sets of PAPI counters, one for 
+	 * each thread.
+	 * @post All threads will now start tracking PAPI statistics.
+	 */
+	static void start(std::vector<papi_t*> &counters);
+	static void stop (std::vector<papi_t*> &counters);
+	static void sum  (std::vector<papi_t*> &counters);
+	
+	// static public constants
+	
+	/** Indicates not to use PAPI counters. */
+	static const uint32_t papi_mode_off = 0;
+	/** Indicates that pre-built L2 and L3 cache performance counters should be used. */
+	static const uint32_t papi_mode_cache = 1;
+	/** Indicates that pre-built branch prediction counters should be used. */
+	static const uint32_t papi_mode_branch = 2;
+	/** Indicates that prebuilt throughput instruction counters should be used. */
+	static const uint32_t papi_mode_cycle = 3;
+	/** Indicates that prebuilt tlb counters should be used. */
+	static const uint32_t papi_mode_tlb = 4;
+	/** Indicates that custom (i.e., user-selected) PAPI events will be specified. */
+	static const uint32_t papi_mode_custom = 5;
+	
+	friend std::ostream& operator<<(std::ostream& os, const papi_t & p);
+	
+protected:
+
+	/**
+	 * Registers a new counter with this object based on a PAPI event. Accepts both 
+	 * preset and native event names.
+	 * @param counter_name The ASCII name of the PAPI event to be registered
+	 * (decoding to an int takes place within this method).
+	 * @param header A human-readable string indicating what is being tracked by
+	 * this hardware counter.
+	 * @post This object is modified to now track a new PAPI event.
+	 */
+	void register_counter( const std::string &counter_name, const std::string &header );
+
+	/** 
+	 * An array containing the actual hardware counter values (recorded at stop() 
+	 * invocations). 
+	 */
+	std::vector< long long > values_; // accessed by friended stream overloading.
+	
+	
+private:
+
+	/** Writes the polymorphic object to an output stream */
+	virtual void to_stream( std::ostream& os ) const = 0;
+
+	/**
+	 * A flag indicating whether or not this set of papi counters is currently 
+	 * being tracked.
+	 */
+	bool papi_started_ = false; 
+	/** An array mapping the indexes used in this object to PAPI hardware counter ids. */
+	std::vector<int> counters_; 
+	/** An array that maps PAPI hardware counters onto human-readable strings. */
+	std::vector<std::string> headers_; 
+};
+
+
+/**
+ * A subset of PAPI hardware counters related to the distribution of instructions.
+ */
+class papi_instructions : public papi_t {
+public:
+
+	papi_instructions();
+	
+	/** Returns the number of load instructions issued. */
+	long long inline load_instructions() const;
+	/** Returns the number of store instructions issued. */
+	long long inline store_instructions() const;
+	/** Returns the number of branch instructions issued. */
+	long long inline branch_instructions() const;
+	/** Returns the total number of instructions issued. */
+	long long inline total_instructions() const;
+
+	/** Returns the number of load instructions issued as a fraction of the total instructions. */
+	double load_instructions_ratio() const;
+	/** Returns the number of store instructions issued as a fraction of the total instructions. */
+	double store_instructions_ratio() const;
+	/** Returns the number of branch instructions issued as a fraction of the total instructions. */
+	double branch_instructions_ratio() const;
+
+private:	
+	void to_stream( std::ostream& os ) const;
+};
+
+
+/**
+ * A subset of PAPI hardware counters related to the distribution of cycles.
+ */
+class papi_cycles : public papi_t {
+
+public:
+
+	papi_cycles();
+
+	/** Returns the total number of cycles spent idling. */
+	inline long long idle_cycles() const;
+	/** Returns the total number of cycles that are utilised. */
+	inline long long utilised_cycles() const;
+	/** Returns the total number of cycles that are stalled. */
+	inline long long stalled_cycles() const;
+	/** Returns the total number of cycles. */
+	inline long long cycles() const;
+	/** Returns the total number of instructions retired. */
+	inline long long instructions() const;
+	/** Returns the fraction of cycles that are stalled due to any resource. */
+	double stalled_cycles_ratio() const;
+	/** Returns the fraction of cycles that are idled. */
+	double idle_cycles_ratio() const;
+	/** Returns the fraction of cycles that are maximally utilised. */
+	double utilised_cycles_ratio() const;
+	/** Returns the average number of cycles spent retiring an instruction (CPI). */
+	double cycles_per_instruction() const;
+
+private:	
+	void to_stream( std::ostream& os ) const;
+};
+
+/**
+ * A subset of PAPI hardware counters related to cache hit performance.
+ */
+class papi_cache : public papi_t {
+
+public:
+	papi_cache();
+
+	/** Returns the number of Level 2 total cache misses. */
+	long long inline l2_cache_misses() const;
+	/** Returns the number of Level 2 total cache accesses. */
+	long long inline l2_cache_accesses() const;
+	/** Returns the number of Level 3 total cache misses. */ 
+	long long inline l3_cache_misses() const;
+	/** Returns the number of Level 3 total cache misses. */
+	long long inline l3_cache_accesses() const;
+
+	/**
+	 * Returns the fraction of L2/L3 cache accesses (both data and instruction) that were 
+	 * missed (because the resource was not available in L2/L3 cache).
+	 */
+	double l2_miss_ratio() const;
+	double l3_miss_ratio() const;
+
+//private:
+	void to_stream( std::ostream& os ) const;
+};
+
+/**
+ * A subset of PAPI hardware counters related to branch prediction performance.
+ */
+class papi_branch : public papi_t {
+
+public:
+	papi_branch();
+
+	/** Returns the number of branch instructions issued. */
+	long long inline branch_instructions() const;
+	/** Returns the number of branch instructions mispredicted. */
+	long long inline branch_misses() const;
+	/** Returns the number of branch instructions correctly predicted. */
+	long long inline branch_hits() const;
+	/** Returns the fraction of conditional branches that were incorrectly predicted. */
+	double misprediction_ratio() const;
+	/** Returns the fraction of conditional branches that were predicted correctly. */
+	double prediction_ratio() const;
+	
+private:
+	void to_stream( std::ostream& os ) const;
+};
+
+/**
+ * A subset of PAPI hardware counters related to transaction lookaside buffer performance.
+ */
+class papi_tlb : public papi_t {
+
+public:
+	papi_tlb();
+
+	/** Returns the total number of data tlb misses. */
+	long long inline data_tlbs() const;
+	/** Returns the total number of instruction tlb misses. */
+	long long inline instruction_tlbs() const;
+	
+private:
+	void to_stream( std::ostream& os ) const;
+};
+
+/**
+ * An set of PAPI hardware counters that use custom-defined PAPI events.
+ */
+class papi_custom : public papi_t {
+
+public:
+	/**
+	 * Constructs a new papi_custom set using a specified set of PAPI events.
+	 * @param event_names Pairs of event names corresponding to the PAPI event 
+	 * names and the human-readable headers for the custom events that should 
+	 * be tracked
+	 * @post Constructs a new instance of a papi_custom set.
+	 */ 
+	papi_custom( const std::vector< std::pair< std::string, std::string > > &event_names );
+	
+private:
+	void to_stream( std::ostream& os ) const;
+};
+
+namespace trigen {
+  //
+  void papi_init(const uint32_t num_cores,
+                 const uint32_t papi_mode,
+                 std::vector<papi_t*> papi_counters[4]);
+  //
+  void papi_finalize(std::vector<papi_t*> papi_counters[4]);
+}
+#endif
