343b9464f1d4be0330a049901a4ac0dc567abbbf
hobywan <hoby.rakotoarivelo@gmail.com>
2019-02-18 19:49:57 +0100

    Improve readability

--- sources/tools.h
+++ sources/tools.h
@@ -27,30 +27,43 @@ namespace trinity { namespace tools {
 /* --------------------------------------------------------------------------- */
 // hash provided key
 uint32_t hash(const uint32_t id);
+
 // get number of digits to print 'num'
 int format(int num);
+
 // print a separator line
 void separator();
+
 // trim left blank space
 void ltrim(std::string& line);
+
 // get end file name of a given path
 std::string basename(const std::string& s);
+
 // check if a file exists
 bool exists(const std::string& path);
+
 // abort if given command option is not supported
 void abort(char option, const char* msg, const optparse::Parser& parser);
+
 // check equality of two c-strings
 bool equals(const char* s1, const char* s2);
+
 // retrieve extension of a file
 std::string getExt(const char* path);
+
 // seek line pointer to a given line number
 std::ifstream& seekToLine(int nb, std::ifstream& file);
+
 // check if provided arg is a digit
 bool isDigit(const char* arg);
+
 // get file name without extension
 std::string rootOf(const std::string& path);
+
 // replace a file extension by another
 std::string replaceExt(const std::string& fname, const std::string& ext);
+
 // display elapsed time
 void showElapsed(Time& tic, const char* msg, int step);
 
